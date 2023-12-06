5dca17b51802e85eec13e44983e9898a6c0710f9
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2023-12-06 13:30:11 +0200

    update backend project

--- backend/app/Http/Resources/OrderResource.php
+++ backend/app/Http/Resources/OrderResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\OrderItemsCollection;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class OrderResource extends JsonResource
@@ -14,11 +15,13 @@ class OrderResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $items=$this->items()->get();
         return [
             "id"=>$this->id,
             "user_id"=>$this->user_id,
             "status"=> $this->status,
-            "billingstatus"=>$this->billingstatus
+            "billingstatus"=>$this->billingstatus,
+            "items"=> OrderItemsResource::collection($items)
         ];
     }
 }
