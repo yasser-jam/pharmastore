a011ae390b5a99ee1746db840b3c4f0a559e0dae
Yasser Jamal-Aldeen <Yasser-Jamal-Al-Deen>
2024-01-02 14:23:47 +0200

    add home to drawer list

--- admin/lib/base/drawer/drawer_list.dart
+++ admin/lib/base/drawer/drawer_list.dart
@@ -9,44 +9,57 @@ class DrawerList extends StatelessWidget {
   @override
   build(ctx) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Padding(
-            padding: const EdgeInsets.all(20),
-            child: Row(children: [
-              const Icon(
-                Icons.mediation,
-                color: Colors.white,
-              ),
-              const SizedBox(width: 10),
-              Text(
-                'Pharm Store',
-                style: GoogleFonts.fugazOne(fontSize: 25, color: Colors.white),
-              ),
-            ])),
-        const Divider(color: Colors.white),
-        const DrawerItem(
-            text: 'Medicines',
-            icon: 0xe60a,
-            link: '/med-list',
-            detailsPage: true),
-        const DrawerItem(
-            text: 'Orders', icon: 0xf0147, link: '/orders', detailsPage: false),
-        const DrawerItem(
-            text: 'Categories', icon: 0xe148, link: '', detailsPage: false),
-        SizedBox(height: 400),
+        Column(
+          children: [
+            Padding(
+                padding: const EdgeInsets.all(20),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.pushNamed(ctx, '/');
+                  },
+                  child: Row(children: [
+                    const Icon(
+                      Icons.mediation,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(width: 10),
+                    Text(
+                      'Pharm Store',
+                      style: GoogleFonts.fugazOne(
+                          fontSize: 25, color: Colors.white),
+                    ),
+                  ]),
+                )),
+            const Divider(color: Colors.white),
+            const DrawerItem(
+                text: 'Medicines',
+                icon: 0xe60a,
+                link: '/med-list',
+                detailsPage: true),
+            const DrawerItem(
+                text: 'Orders',
+                icon: 0xf0147,
+                link: '/orders',
+                detailsPage: false),
+            const DrawerItem(
+                text: 'Categories', icon: 0xe148, link: '', detailsPage: false),
+          ],
+        ),
         ListTile(
           onTap: () {
             Navigator.pushNamed(ctx, '/login');
           },
           contentPadding:
-              const EdgeInsets.symmetric(vertical: 0, horizontal: 30),
+              const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
           leading: const Icon(
             Icons.login,
             color: Colors.white,
           ),
-          title: Text(
+          title: const Text(
             'Log out',
-            style: const TextStyle(color: Colors.white),
+            style: TextStyle(color: Colors.white),
           ),
         ),
       ],
